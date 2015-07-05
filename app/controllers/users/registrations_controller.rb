15608a85ebd354e421757905f70e98e2e456fb61
Josue Martinez <josuedaniel831@yahoo.com>
2015-07-05 15:39:36 +0000

    Fixed problems found in user.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,14 @@
-class Users::RegistrationsController < Devise::RegistrationsController 
-  
+class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super do |resource|
-      if params[:plan]  
+      if params[:plan]
         resource.plan_id = params[:plan]
-        if resouce.plan_id == 2
+        if resource.plan_id == 2
           resource.save_with_payment
         else
-          resource.
-          end
+          resource.save
         end
-     end
+      end
+    end
   end
 end
